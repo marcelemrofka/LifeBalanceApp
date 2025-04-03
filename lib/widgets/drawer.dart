6f7b9c004ff7eda528e9675f71482fbfb85b1b3a
IsaBertocco <111938842+isabertocco05@users.noreply.github.com>
2025-04-03 15:30:35 -0300

    drawer com info user

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -6,21 +6,28 @@ class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      backgroundColor: Color(0xFF43644A),
+      backgroundColor: Color(0xFF43644A), // Fundo verde
       child: ListView(
+        padding: EdgeInsets.zero,
         children: [
-          DrawerHeader(
-          child: Align(
-            alignment: Alignment.bottomLeft,
-            child: Row(
-              children: [
-                Image.asset('../lib/images/logo-folha.png', width: 35, height: 35),
-                SizedBox(width: 10),
-                Text('Menu', style: TextStyle(fontSize: 18, color: Colors.white)),
-              ],
-            )
+          UserAccountsDrawerHeader(
+            decoration: BoxDecoration(
+              color: Color(0xFF43644A), // Fundo verde corrigido
+            ),
+            accountEmail: Text("user@mail.com", style: TextStyle(color: Colors.white)),
+            accountName: Text("Seu Zé", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+            currentAccountPicture: CircleAvatar(
+              backgroundColor: Colors.white,
+              child: Image.asset('assets/images/logo-folha.png', width: 35, height: 35),
+            ),
           ),
-          )
+          ListTile(
+            leading: Icon(Icons.home, color: Colors.white),
+            title: Text('Início', style: TextStyle(color: Colors.white)),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
         ],
       ),
     );
