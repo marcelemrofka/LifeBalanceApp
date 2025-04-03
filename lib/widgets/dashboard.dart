12cd14f478880711cd6685b51c4faa0c868f10ad
IsaBertocco <111938842+isabertocco05@users.noreply.github.com>
2025-04-03 11:12:09 -0300

    Dashboard
    precisei alterar toda a estrutura

--- lib/widgets/dashboard.dart
+++ lib/widgets/dashboard.dart
@@ -1,8 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:percent_indicator_premium/percent_indicator_premium.dart';
+import 'package:percentages_with_animation/percentages_with_animation.dart';
 
-class Dashboard extends StatelessWidget {
+class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  final double carboidrato = 40;
+  final double proteina = 30;
+  final double gordura = 20;
+  final double fibra = 30;
+  final int calorias = 1550;
+  final double percentCaloriasConsumidas = (0.72 * 100);
+
+  Widget progressBar(String label, double value, double maxValue) {
+    return Column(
+      children: [
+        Text(label),
+        const SizedBox(height: 4),
+        SizedBox(
+          width: 100,
+          child: LinearPercentage(
+            currentPercentage: (value / maxValue) * 100,
+            maxPercentage: 100,
+            backgroundHeight: 10,
+            percentageHeight: 10,
+            leftRightText: LeftRightText.none, // textos laterais
+            backgroundDecoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: Colors.grey.shade300,
+            ),
+            percentageDecoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: Colors.green,
+            ),
+          ),
+        ),
+        const SizedBox(height: 4),
+        Text('${value.toStringAsFixed(0)}g',
+            style: const TextStyle(fontSize: 12, color: Colors.black)),
+      ],
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -10,74 +53,76 @@ class Dashboard extends StatelessWidget {
       width: double.infinity,
       height: 250,
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Expanded(
-            // https://pub.dev/packages/percentages_with_animation
-            flex: 3,
-            child: Column(
-              children: [
-                Expanded(
-                  child: Column(
-                    children: [
-                      Text('Carboidratos'),
-                      Text('%Carboidratos'), 
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: Column(
-                    children: [
-                      Text('Proteina'),
-                      Text('%Proteina'), 
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
 
-          Expanded(
-             // https://pub.dev/packages/percentages_with_animation
-            flex: 5,
-            child: Center(
-              child: Container(
-                width: 160, 
-                height: 160,
-                decoration: const BoxDecoration(
-                  color: Color(0xFF43644A),
-                  shape: BoxShape.circle,
-                ),
+          // Carb e Fibras
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+                progressBar("Carboidratos", carboidrato, 100),
+                progressBar("Fibras", fibra, 100),
+            ],
+          ),
+             
+            
+          // Circulo
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                width: 160,
+                height: 160, 
+                decoration: const BoxDecoration(color: Color(0xFF43644A), shape: BoxShape.circle,),
               ),
-            ),
-          ),
+              
+              CircularPercentage(
+                  currentPercentage: percentCaloriasConsumidas, 
+                  maxPercentage: 100, 
+                  size: 160,
+                  percentageStrokeWidth: 7, 
+                  backgroundStrokeWidth: 1,
+                  percentageColor: Colors.green.shade400,
+                  centerText: '', // precisa ser vazio para ser sobreposto
+                ),
 
-          Expanded(
-            flex: 3,
-            child: Column(
-              children: [
-                Expanded(
-                  child: Column(
-                    children: [
-                      Text('Gordura'),
-                      Text('%Gordura'), 
-                    ],
-                  ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '${calorias.toStringAsFixed(0)} kcal',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                    ),
+                    const Text("Consumidas", style: TextStyle(fontSize: 10, color: Colors.white)),
+                    SizedBox(height: 15),
+                    Text(
+                      "Você consumiu ${percentCaloriasConsumidas.toStringAsFixed(0)}%",
+                      style: TextStyle(fontSize: 10, color: Colors.white),
+                    ),
+                  ],
                 ),
-                Expanded(
-                  child: Column(
-                    children: [
-                      Text('Fibras'),
-                      Text('%Fibras'), 
-                    ],
-                  ),
-                ),
-              ],
-            ),
+            ],
+                
+    
+              // ],
+            // ),
           ),
+           
+          // Proteinas e Gorduras 
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              progressBar("Proteínas", proteina, 100),
+              progressBar("Gorduras", gordura, 100)
+            ],
+          ),
+          
         ],
       ),
     );
   }
 }
-
-
